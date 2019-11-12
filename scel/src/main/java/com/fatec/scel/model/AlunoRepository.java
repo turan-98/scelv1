c8432ef7792d57cb9c59887e118193083c812a51
Samuel Santana <35262710+turan-98@users.noreply.github.com>
2019-11-12 09:55:53 -0300

    Add files via upload

--- scel/src/main/java/com/fatec/scel/model/AlunoRepository.java
+++ scel/src/main/java/com/fatec/scel/model/AlunoRepository.java
@@ -1,5 +1,7 @@
 package com.fatec.scel.model;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,12 @@ import org.springframework.stereotype.Repository;
 public interface AlunoRepository extends CrudRepository<Aluno, String> {
 		
 	@Query("SELECT l FROM Aluno l WHERE l.ra = :ra")
-    public Livro findByRa(@Param("ra") String ra);
+    public Aluno findByRa(@Param("ra") String ra);
+
+	public Optional<Aluno> findById(Long id);
+
+	public void deleteById(Long id);
+
+	public void deleteByRa(String ra);
 		
 }
